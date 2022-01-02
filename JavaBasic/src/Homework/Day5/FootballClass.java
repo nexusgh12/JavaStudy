1d2ab02c70eff3e3dfc842e7395472acd0f7c01b
nexusgh12 <70796139+nexusgh12@users.noreply.github.com>
2022-01-03 01:24:50 +0900

    Football Game 수정
    기존 가위바위보 형식에서 실제 축구 경시 형식으로 변경

--- JavaBasic/src/Homework/Day5/FootballClass.java
+++ JavaBasic/src/Homework/Day5/FootballClass.java
@@ -1,72 +1,106 @@
 package Homework.Day5;
 
 import java.security.PrivateKey;
+import java.util.Random;
 
 public class FootballClass {
     public static void main(String[] args) {
-        //Football f1 = new Football("한국", "손흥민");
+        String[] t1Players = {"손흥민", "H.케인", "루카스 모우라", "G.로셀소", "O.스킵", "P.호이비에르", "B.데이비스", "E.다이어", "C.로메로", "에메르송", "H.요리스"};
+        String[] t2Players = {"C.호날두", "E.카바니", "L.쇼", "프레드", "S.맥토미니", "B.페르난데스", "A.완 비사카", "H.맥과이어", "R.바란", "V.린델로프", "D.데헤아"};
 
-        //f1.play();
-
-        Team_Football t1 = new Team_Football("토트넘", "손흥민");
-        Team_Football t2 = new Team_Football("울버햄튼", "황희찬");
+        Team_Football t1 = new Team_Football("토트넘", "손흥민", t1Players);
+        Team_Football t2 = new Team_Football("맨유", "C.호날두", t2Players);
 
         Football football = new Football(t1, t2);
 
+        football.gameStart();
         football.gameResult();
-        
+
     }
 
 }
 
 class Player_Football {
     private String playerName;
-
+    private String[] players;
 
     public Player_Football() {
 
     }
 
-    public Player_Football(String playerName) {
+    public Player_Football(String playerName, String[] players) {
         this.playerName = playerName;
+        this.players = players;
     }
 
+    
+
     public String getPlayerName() {
-        return playerName;
+        return players[(int)(Math.random()*players.length)];
     }
 
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
 
+    public String[] getPlayers(){
+        return players;
+    }
+
+    public void setPlayers(String[] players){
+        this.players = players;
+    }
+
+
 }
 
 class Team_Football extends Player_Football {
     private String teamName;
     private String result;
-    static String[] rsp = new String[] { "가위", "바위", "보" };
+    private boolean attack = false;
+    private boolean defense = false;
+    private int score = 0;
 
     public Team_Football() {
 
     }
 
-    public Team_Football( String teamName, String playerName) {
-        super(playerName);
+    public Team_Football(String teamName, String playerName, String[] players) {
+        super(playerName, players);
         this.teamName = teamName;
     }
 
-    public String getTeamName() {
-        return teamName;
+    public void randomAttack() {
+        Random random = new Random();
+        this.attack = random.nextBoolean();
     }
 
-    public void setPlayerName(String teamName) {
-        this.teamName = teamName;
+    public void randomDefense() {
+        Random random = new Random();
+        this.defense = random.nextBoolean();
     }
 
-    public void play() {
-        int i = (int) (Math.random() * 3);
-        setResult(rsp[i]);
-        System.out.printf("%s - %s : %s%n", getTeamName(), getPlayerName() , getResult());
+    public void shooting() {
+        randomAttack();
+        if (attack) {
+            randomDefense();
+            if (defense) {
+                System.out.printf("[%s]: %s(이)가 슈팅에 실패했습니다.%n", getTeamName(), getPlayerName());
+            } else {
+                score += 1;
+                System.out.printf("[%s]: %s(이)가 득점 했습니다.%n", getTeamName(), getPlayerName());
+            }
+        } else {
+            System.out.printf("[%s]: %s(이)가 공을 빼았겼습니다.%n", getTeamName(), getPlayerName());
+        }
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public String getResult() {
@@ -77,11 +111,20 @@ class Team_Football extends Player_Football {
         this.result = result;
     }
 
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setPlayerName(String teamName) {
+        this.teamName = teamName;
+    }
+
 }
 
 class Football extends Team_Football {
 
     private Team_Football t1, t2;
+    private Team_Football[] teams;
     private String winner;
 
     public Football() {
@@ -91,48 +134,37 @@ class Football extends Team_Football {
     public Football(Team_Football t1, Team_Football t2) {
         this.t1 = t1;
         this.t2 = t2;
-
-        this.t1.play();
-        this.t2.play();
+        this.teams = new Team_Football[] { t1, t2 };
 
     }
 
-    public void gameResult(){
-        if (t1.getResult() == rsp[0]) {
-            if (t2.getResult() == rsp[0]) {
-                winner = "무승부";
-            } else if (t2.getResult() == rsp[1]) {
-                winner = t2.getTeamName();
-            } else if (t2.getResult() == rsp[2]) {
-                winner = t1.getTeamName();
-            }
-        } else if (t1.getResult() == rsp[1]) {
-
-            if (t2.getResult() == rsp[0]) {
-                winner = t1.getTeamName();
-            } else if (t2.getResult() == rsp[1]) {
-                winner = "무승부";
-            } else if (t2.getResult() == rsp[2]) {
-                winner = t2.getTeamName();
-            }
-        } else if (t1.getResult() == rsp[2]) {
-
-            if (t2.getResult() == rsp[0]) {
-                winner = t2.getTeamName();
-            } else if (t2.getResult() == rsp[1]) {
-                winner = t1.getTeamName();
-            } else if (t2.getResult() == rsp[2]) {
-                winner = "무승부";
-            }
+    public void gameStart() {
+        for (int i = 0; i <= 90; i += (int) (Math.random() * 10)) {
+            int selectTeam = (int) (Math.random() * 2);
+            System.out.printf("%n[%d 분]%n", i);
+            teams[selectTeam].shooting();
         }
 
-        if (!winner.equals("무승부"))
-            System.out.printf("승리팀은 %s입니다.",  winner);
-        else
-        System.out.printf("게임 결과는 %s입니다.",  winner);
+        if (t1.getScore() > t2.getScore()) {
+            winner = t1.getTeamName();
+        } else if (t1.getScore() == t2.getScore()) {
+            winner = "무승부";
+        } else if (t1.getScore() < t2.getScore()) {
+            winner = t2.getTeamName();
+        }
+    }
+
+    public void gameResult() {
+        System.out.println();
+        System.out.println("-------------경기 결과-------------");
+        System.out.printf("스코어 [%s 팀] %d : [%s 팀] %d%n", t1.getTeamName(), t1.getScore(), t2.getTeamName(),
+                t2.getScore());
+
+        if (!winner.equals("무승부")) {
+            System.out.printf("승리팀은 %s입니다.%n", winner);
+        } else {
+            System.out.printf("경기 결과는 %s 입니다.%n", winner);
+        }
     }
 
 }
-
-
-
